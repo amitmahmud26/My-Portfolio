b7b4bab36f8edf6b5b1ccd4e823fb987a8efb3db
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2024-06-10 18:42:27 +0600

    updated my work section

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,9 +1,12 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project1_img from '../assets/project_1.png'
+import project2_img from '../assets/project_2.png'
+import project3_img from '../assets/project_3.png'
+import project4_img from '../assets/project_4.png'
+import project5_img from '../assets/project_5.png'
+import project6_img from '../assets/project_6.png'
+import project7_img from '../assets/project_7.png'
+import project8_img from '../assets/project_8.png'
+import project9_img from '../assets/project_9.png'
 
 const mywork_data = [
     {
@@ -36,6 +39,21 @@ const mywork_data = [
         w_name:"Web design",
         w_img:project6_img
     },
+    {
+        w_no:7,
+        w_name:"Python",
+        w_img:project7_img
+    },
+    {
+        w_no:8,
+        w_name:"Web design",
+        w_img:project8_img
+    },
+    {
+        w_no:9,
+        w_name:"Java Swing",
+        w_img:project9_img
+    }
 ]
  
 export default mywork_data;
