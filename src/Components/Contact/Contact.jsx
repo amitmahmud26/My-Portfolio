6b3c4c5e9d81c690dddab5a18f398a04a56b26ea
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2024-06-12 21:10:32 +0600

    send msz available with sweet alart

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,6 +5,34 @@ import mail_icon from '../../assets/mail_icon.svg'
 import location_icon from '../../assets/location_icon.svg'
 import call_icon from '../../assets/call_icon.svg'
 const Contact = () => {
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "3bb6607c-e27d-4a06-bd53-87851ec40bdb");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      Swal.fire({
+        title: "Success!",
+        text: "Your message has been sent!",
+        icon: "success"
+      });
+    }
+  };
+
   return (
     <div id='contact' className='contact'>
       <div className="contact-title">
@@ -27,7 +55,7 @@ const Contact = () => {
             </div>
           </div>
         </div>
-        <form action="" className="contact-right">
+        <form onSubmit={onSubmit} action="" className="contact-right">
           <label htmlFor="">Your Name</label>
           <input type="text" placeholder='Enter your name.' name='name'/>
           <label htmlFor="">Your Email</label>
