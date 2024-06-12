42c0d6ef2f3621f1085bc5b81a32811b5e4936d7
Amit Mahmud Sabbir <amitsclan26@gmail.com>
2024-06-12 14:43:58 +0600

    updated logo

--- src/Components/Navber/Navbar.jsx
+++ src/Components/Navber/Navbar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navber.css'
 import logo from '../../assets/logo.png'
-import download_logo from '../../assets/download.png'
+import underline from '../../assets/nav_underline.svg'
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState('home');
+
   return (
     <div className='navbar'>
         <img src={logo} alt="" />
         <ul className="nav-menu">
-            <li>Home</li>
-            <li>Abour Me</li>
-            <li>Services</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
+            <li><p onClick={()=>setMenu("home")}>Home</p>{menu==="home"?<img src={underline} alt="" />:<></>}</li>
+            <li><p onClick={()=>setMenu("about")}>About Me</p>{menu==="about"?<img src={underline} alt="" />:<></>}</li>
+            <li><p onClick={()=>setMenu("services")}>Services</p>{menu==="services"?<img src={underline} alt="" />:<></>}</li>
+            <li><p onClick={()=>setMenu("work")}>Portfolio</p>{menu==="work"?<img src={underline} alt="" />:<></>}</li>
+            <li><p onClick={()=>setMenu("contact")}>Contact</p>{menu==="contact"?<img src={underline} alt="" />:<></>}</li>
         </ul>
         <a href="https://drive.google.com/uc?export=download&id=1FeuAdT8d-BOro34lHhepSMUkwvTUD3Tc"><div className="nav-connect">Download Resume ↓</div></a>
     </div>
